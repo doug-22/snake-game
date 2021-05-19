0fb3fde78b333848160f25573a27a0b5b1d7fae3
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-05-19 18:45:22 -0300

    criando snake

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
 
 function criarBG() {
-  context.fillStyle = "lightgrenn";
+  context.fillStyle = "#00d287";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarSnake() {
+  for( i = 0; i < snake.length; i++){
+    context.fillStyle = "#4b758e";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
 criarBG();
+criarSnake();
